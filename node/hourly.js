db349b929dffc8f9f70e4e3485b0e560e5c165cc
MidStrLK <stepma@list.ru>
2016-09-15 15:53:09 +0300

    4

--- node/hourly.js
+++ node/hourly.js
@@ -76,7 +76,7 @@ function calcAccuHourly(data){
 
     var res = [],
         i = 0,
-        nowTime = (new Date()).getHours();
+        nowTime = (new Date()).getHours() + 7;
 
     while(nowTime < 24){
 
